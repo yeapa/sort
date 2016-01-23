59b4938e9fddd3b4faa3ecc203e734a481fda4f9
yeapa <1534873570@qq.com>
2016-01-23 22:36:04 +0800

    gitignoge add

--- sort.py
+++ sort.py
@@ -1,20 +1,24 @@
-import show.showList as  displayList
+from show import  showList
 import copy
-def exchange(aList,a,b):
-    aList[a],aList[b]=aList[b],aList[a]
+
+
+def exchange(aList, a, b):
+    aList[a], aList[b] = aList[b], aList[a]
     pass
 
+
 def insertSort(aList):
-    display_temp=[];
+    display_temp = []
     for i in xrange(1, len(aList)):
-        for j in xrange(i,0,-1):
-            if aList[j] < aList[j-1]:
-                exchange(aList,j, j-1)
-        display_temp=display_temp.append(copy.deepcopy(aList))
-    displayList(display_temp)
+        for j in xrange(i, 0, -1):
+            if aList[j] < aList[j - 1]:
+                exchange(aList, j, j - 1)
+        display_temp.append(copy.copy(aList))
+    showList(display_temp)
     return aList
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     aList = [1, 5, 3, 2]
     sortedList = insertSort(aList)
     print sortedList
